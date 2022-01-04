7c60157d61a2244f052301999865fb0753942f20
Francisco <oficialkikolms123@gmail.com>
2022-01-04 01:50:10 +0000

    Basic calculator Working

--- calculinator/index.js
+++ calculinator/index.js
@@ -1,21 +1,73 @@
 //Create an array put all in the array, when he hits the "=" make the operation
 
-var values = [];
+var values = ["+"];
 var display = document.getElementById("total");
 var equal = document.getElementById("equal");
 
 function getValue(clickedValue){
+    str = "";
     values.push(clickedValue);
-    display.innerHTML = values.toString();
-    console.log(values.toString().replace('3', '1'));
+    for(let i = 0; i < values.length; i++){
+        str += values[i];
+    }
+    display.innerHTML = str;
+    console.log(str);
 }
 
-function displayOnScreen(){
-    display.innerHTML = values.toString().replace(",", "");
+function clearDisplay(){
+    values = [];
+    display.innerHTML = "0";
 }
-
 
 //Need to make a little algorithm to go to the first operator
 equal.addEventListener("click", () =>{
-    displayOnScreen();
+    var res = 0;
+    var firstNumber = "", secondNumber = "";
+    var operator = "";
+    //Separate the first Number and second number and the operator
+    for(let i = 0; i < values.length; i++){
+        if(operator == ""){
+            firstNumber += values[i].toString();
+        }else{
+            secondNumber += values[i].toString();
+        }
+
+        console.log(values[i]);
+
+        if(values[i].toString() === "+" && operator == ""){
+            operator = "+";
+        } else if(values[i].toString() == "-"  && operator == ""){
+            operator = "-";
+        } else if(values[i].toString() == "*"  && operator == ""){
+            operator = "*";
+        }else if(values[i].toString() == "/"  && operator == ""){
+            operator = "/";
+        }
+    }
+
+    console.log("1stNumber: " + firstNumber);
+    //Remove the operator from the firstNumber
+    firstNumber = firstNumber.substring(0, firstNumber.length -1);
+
+
+    console.log("operator: " + operator);
+    console.log("1stNumber: " + firstNumber);
+    console.log("2ndNumber: " + secondNumber);
+
+    switch(operator){
+        case '+':
+            res = parseInt(firstNumber) + parseInt(secondNumber);
+        break;
+        case '-':
+            res = parseInt(firstNumber) - parseInt(secondNumber);
+        break;
+        case '*':
+            res = parseInt(firstNumber) * parseInt(secondNumber);
+        break;
+        case '/':
+            res = parseInt(firstNumber) / parseInt(secondNumber);
+        break;
+    }
+
+    display.innerHTML = res;
 });
